f96068c97c0a4fe404ba13bba8cddda8123deb0a
Maxim Baduk <mb2474@cornell.edu>
2022-03-09 16:51:09 -0500

    Resolve merge conflicts with mobile-build branch. Update project name references from "SL" (ShipLab) to "SW" (SwitchWitch).

--- source/SWGameScene.h
+++ source/SWGameScene.h
@@ -21,24 +21,23 @@
 #include "SWInputController.h"
 
 /**
- * This class is the primary gameplay constroller for the demo.
- *
- * A world has its own objects, assets, and input controller.  Thus this is
- * really a mini-GameEngine in its own right.  As in 3152, we separate it out
- * so that we can have a separate mode for the loading screen.
+ * This class is the primary gameplay constroller.
  */
-class GameScene : public cugl::Scene2 {
+class GameScene : public cugl::Scene2
+{
 protected:
     /** The asset manager for this game mode. */
     std::shared_ptr<cugl::AssetManager> _assets;
-    
+
     // CONTROLLERS are attached directly to the scene (no pointers)
     /** The controller to manage the ship */
     InputController _input;
-    
+
     // MODELS should be shared pointers or a data structure of shared pointers
+    /** The JSON value with all of the constants */
     std::shared_ptr<cugl::JsonValue> _constants;
-    
+
+#pragma mark State Varibales
     /** Possible states of the level.
      * Name of state is describing what the current state of the game is waiting for.
      * For example, the selecting_unit state means the game is waiting for the player to select a unit.
@@ -47,86 +46,100 @@ protected:
     {
         SELECTING_UNIT,
         SELECTING_SWAP,
+        CONFIRM_SWAP,
         ANIMATION
     };
-    
+
     /** The current state of the selection process*/
     State _currentState;
-    
-    Square _selectedSquare;
-    Square _swappingSquare;
-    
-    /** The active units on the board*/
-    vector<Unit> _unit_vec;
-    /** The active (poly2) units on the board*/
-    vector<Poly2> _units;
-    /** The active (poly2) squares on the board*/
-    vector<Poly2> _squares;
-    
-    // VIEW
-    std::shared_ptr<cugl::scene2::PolygonNode> _boardNode;
-    std::shared_ptr<cugl::scene2::SceneNode> _guiNode;
-    
-    //TEXTURES SQUARES
-    std::shared_ptr<cugl::Texture> _squareTexture;
-    std::shared_ptr<cugl::Texture> _selectedSquareTexture;
-    std::shared_ptr<cugl::Texture> _attackedSquareTexture;
-    std::shared_ptr<cugl::Texture> _swapSquareTexture;
-    
-    //TEXTURES UNITS
-    std::shared_ptr<cugl::Texture> _redUnitTexture;
-    std::shared_ptr<cugl::Texture> _blueUnitTexture;
-    std::shared_ptr<cugl::Texture> _greenUnitTexture;
-    
-    //TEXTURES SPECIAL UNITS - DIAGONAL
-    std::shared_ptr<cugl::Texture> _diagonalBlueTexture;
-    std::shared_ptr<cugl::Texture> _diagonalGreenTexture;
-    std::shared_ptr<cugl::Texture> _diagonalRedTexture;
-    
-    //TEXTURES SPECIAL UNITS - TWO-FORWARD
-    std::shared_ptr<cugl::Texture> _twoForwardBlueTexture;
-    std::shared_ptr<cugl::Texture> _twoForwardGreenTexture;
-    std::shared_ptr<cugl::Texture> _twoForwardRedTexture;
-    
-    //TEXTURES SPECIAL UNITS - THREE-WAY
-    std::shared_ptr<cugl::Texture> _threeWayBlueTexture;
-    std::shared_ptr<cugl::Texture> _threeWayGreenTexture;
-    std::shared_ptr<cugl::Texture> _threeWayRedTexture;
-    
-    /** The pair with the first = squarePoly and second = unitPoly correspondingly*/
-    typedef std::pair<Poly2, Poly2> values;
-    
-    /** Self-defined hash function for square */
-    struct square_hash
-    {
-        std::size_t operator () (Square const& s) const
-        {
-            cugl::Vec2 vec2 = s.getPosition();
-            size_t rowHash = std::hash<int>()(vec2.x);
-            size_t colHash = std::hash<int>()(vec2.y) << 1;
-            return rowHash ^ colHash;
-        }
-    };
-    /** A map with key = Square, and value = pair<sqaurePoly, unitPoly> correspondingly */
-    std::unordered_map<Square, values, square_hash> _map;
-    
-    
-    /** The board*/
-    std::shared_ptr<Board> _board;
-    
+    /** The current number of turns left for the player */
     int _turns;
+    /** The current score of the player */
     int _score;
-    
+    /** The previous score of the player */
+    int _prev_score;
+
+
+    /** The number of colors killed */
+    int _attackedColorNum;
+    /** The number of basic units killed */
+    int _attackedBasicNum;
+    /** The number of special units killed */
+    int _attackedSpecialNum;
+
+#pragma mark -
+#pragma mark Model Variables
+    /** The board */
+    shared_ptr<Board> _board;
+    /** The replacement units,unitNodes list */
+    shared_ptr<Board> _replacementBoard;
+
+    std::vector<std::pair<std::shared_ptr<Unit>, std::shared_ptr<scene2::PolygonNode>>> _replacementList;
+    /** Square that is currently being selected by the player */
+    shared_ptr<Square> _selectedSquare;
+    /** Square that is currently being selected by the player to swap with the selectedSquare */
+    shared_ptr<Square> _swappingSquare;
+
+#pragma mark -
+#pragma mark View Variables
+    // VIEW
+    std::shared_ptr<cugl::scene2::AnchoredLayout> _layout;
+    std::shared_ptr<cugl::scene2::PolygonNode> _boardNode;
+    std::shared_ptr<cugl::scene2::PolygonNode> _replacementBoardNode;
+    std::shared_ptr<cugl::scene2::SceneNode> _guiNode;
+    // std::shared_ptr<cugl::scene2::SceneNode> _boardNodeS;
+    // std::shared_ptr<cugl::scene2::SceneNode> _replacementBoardNodeS;
+
+    int _replacementListLength;
     // VIEW items are going to be individual variables
     // In the future, we will replace this with the scene graph
     /** The backgrounnd image */
     std::shared_ptr<cugl::Texture> _background;
     /** The text with the current remaining turns */
-    std::shared_ptr<cugl::TextLayout> _turn_text;
+    std::shared_ptr<cugl::scene2::Label> _turn_text;
     /** The text with the current score */
-    std::shared_ptr<cugl::TextLayout> _score_text;
-    
-    
+    std::shared_ptr<cugl::scene2::Label> _score_text;
+    /** The text above the replacement board */
+    std::shared_ptr<cugl::scene2::Label> _replace_text;
+    // std::shared_ptr<cugl::TextLayout> _score_text;
+    /** Win/Lose text */
+    std::shared_ptr<cugl::scene2::Label> _endgame_text;
+
+
+    std::shared_ptr<cugl::TextLayout> _winLoseText;
+    vector<shared_ptr<Square>> _attacked_squares;
+
+    bool hasLost = false;
+#pragma mark -
+#pragma mark Texture Variables
+    // TEXTURES SQUARES
+    std::shared_ptr<cugl::Texture> _squareTexture;
+    std::shared_ptr<cugl::Texture> _selectedSquareTexture;
+    std::shared_ptr<cugl::Texture> _attackedSquareTexture;
+    std::shared_ptr<cugl::Texture> _swapSquareTexture;
+
+    // TEXTURES UNITS
+    std::shared_ptr<cugl::Texture> _redUnitTexture;
+    std::shared_ptr<cugl::Texture> _blueUnitTexture;
+    std::shared_ptr<cugl::Texture> _greenUnitTexture;
+
+    // TEXTURES SPECIAL UNITS - DIAGONAL
+    std::shared_ptr<cugl::Texture> _diagonalBlueTexture;
+    std::shared_ptr<cugl::Texture> _diagonalGreenTexture;
+    std::shared_ptr<cugl::Texture> _diagonalRedTexture;
+
+    // TEXTURES SPECIAL UNITS - TWO-FORWARD
+    std::shared_ptr<cugl::Texture> _twoForwardBlueTexture;
+    std::shared_ptr<cugl::Texture> _twoForwardGreenTexture;
+    std::shared_ptr<cugl::Texture> _twoForwardRedTexture;
+
+    // TEXTURES SPECIAL UNITS - THREE-WAY
+    std::shared_ptr<cugl::Texture> _threeWayBlueTexture;
+    std::shared_ptr<cugl::Texture> _threeWayGreenTexture;
+    std::shared_ptr<cugl::Texture> _threeWayRedTexture;
+
+#pragma mark -
+
 public:
 #pragma mark -
 #pragma mark Constructors
@@ -137,7 +150,7 @@ public:
      * This allows us to use the object without a heap pointer.
      */
     GameScene() : cugl::Scene2() {}
-    
+
     /**
      * Disposes of all (non-static) resources allocated to this mode.
      *
@@ -145,12 +158,12 @@ public:
      * static resources, like the input controller.
      */
     ~GameScene() { dispose(); }
-    
+
     /**
      * Disposes of all (non-static) resources allocated to this mode.
      */
     void dispose() override;
-    
+
     /**
      * Initializes the controller contents, and starts the game
      *
@@ -162,9 +175,8 @@ public:
      *
      * @return true if the controller is initialized properly, false otherwise.
      */
-    bool init(const std::shared_ptr<cugl::AssetManager>& assets);
+    bool init(const std::shared_ptr<cugl::AssetManager> &assets);
 
-    
 #pragma mark -
 #pragma mark Gameplay Handling
     /**
@@ -185,8 +197,8 @@ public:
      *
      * @param batch     The SpriteBatch to draw with.
      */
-    void render(const std::shared_ptr<cugl::SpriteBatch>& batch) override;
-    
+    void render(const std::shared_ptr<cugl::SpriteBatch> &batch) override;
+
     /**
      * Resets the status of the game so that we can play again.
      */
@@ -203,40 +215,22 @@ private:
      * @return    The score of this attack
      */
     int calculateScore(int colorNum, int basicUnitsNum, int specialUnitsNum);
-    
-    /**
-     * Returns the tag of a square node based on the position of the square.
-     *
-     * The children of a node are organized by tag number.
-     * This method returns the appropriate tag number given the position of a square.
-     *
-     * @param x     The x position of the square
-     * @param y     The y position of the square
-     * @return    The tag of the square node
-     */
-    int squarePosToTag(const int x, const int y);
 
     /**
-     * Returns the tag of a square node based on the position of the square.
+     * Generate a unit with random color, attack, and direction on the given square.
      *
-     * The children of a node are organized by tag number.
-     * This method returns the appropriate tag number given the position of a square.
+     * @param sq    The given square
+     * @param squareNode    The given squareNode
      *
-     * @param position The position of the square
-     * @return    The tag of the square node
      */
-    int squarePosToTag(Vec2 position) {
-        return squarePosToTag(position.x, position.y);
-    }
+    void generateUnit(shared_ptr<Square> sq, shared_ptr<scene2::PolygonNode> squareNode);
 
-    /**
-     * Returns the position of a square node based on the tag number of a square node.
-     * The children of a node are organized by tag number.
-     *
-     * @param tagNum The tag number of the node
-     * @return The position of the square in a Vec2
-     */
-    cugl::Vec2 tagToSquarePos(const int tagNum);
+    void replaceUnit(shared_ptr<Square> sq, shared_ptr<scene2::PolygonNode> squareNode);
+
+    void replaceUnitNoDelete(shared_ptr<Square> sq, shared_ptr<scene2::PolygonNode> squareNode, int i);
+
+    std::pair<std::shared_ptr<Unit>, std::shared_ptr<scene2::PolygonNode>> generateUnitDontSet();
+
 };
 
 #endif /* __SW_GAME_SCENE_H__ */
